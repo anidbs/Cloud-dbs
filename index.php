dcfabe9106f5a59dabd98d14047e680abb25c0cb
anidbs <39301281+anidbs@users.noreply.github.com>
2018-05-15 13:02:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
  <body>
  <?php echo '<p>Hello World! Welcome to cloud!</p>'; ?> 
   </br>
- <a href="http://www.domstechblog.com/should-you-set-up-your-office-in-the-cloud"> Cloud image </a>
+ <p>Click on the link:<a href="http://www.domstechblog.com/should-you-set-up-your-office-in-the-cloud"> Cloud image </a> for opening a different website.</p>
  </body>
 </html>
