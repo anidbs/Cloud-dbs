c5ae2c1107db711f3904cbf471dd28e3ee83b495
anidbs <39301281+anidbs@users.noreply.github.com>
2018-05-15 13:03:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
   <title>PHP Test</title>
  </head>
  <body>
+  <h2>
  <?php echo '<p>Hello World! Welcome to cloud!</p>'; ?> 
+  </h2>
   </br>
  <p>Click on the link:<a href="http://www.domstechblog.com/should-you-set-up-your-office-in-the-cloud"> Cloud image </a> for opening a different website.</p>
  </body>
